fea5b4e35c1b4a2ce72afdf822e27132feb7ef6d
Anubhooti Pandey <anubhootipandey05@gmail.com>
2023-11-25 21:05:28 +0530

    first commit

---
+++ src/Data/Data.js
@@ -0,0 +1,22 @@
+export const accordionData = [
+    {
+      title: "Explain HTML5 Web storage.",
+      content: `Web Storage in HTML Web storage allows web applications to store data locally in the user's browser. Application data had to be preserved in cookies and sent with each server request before HTML5. Web storage, which is more secure, allows for the local storing of large amounts of data without impairing a website's operation.`,
+    },
+    {
+      title: "Explain DOM (Document Object Model).",
+      content: `The DOM enables programmatic reading, manipulation, and modification of an XML document. The XMLReader class also reads XML, but it does so in a non-cached, forward-only, read-only manner.`,
+    },
+    {
+      title: "What do you know about pair programming?",
+      content: `Pair software programming is a sequential model in which two programmers collaborate on the same computer. While the driver is writing code, the observer or navigator reviews each line of programming as it is entered. Positions for software developers frequently change.`,
+    },
+    {
+      title: "Define NPM (Node Package Manager). ",
+      content: `The word NPM (Node Package Manager) tends to come from when npm was first created as a package manager for Node.js. All npm packages are defined in package.json files.`,
+    },
+    {
+      title: "Explain W3C (World Wide Consortium).",
+      content: `A global organization made up of member organizations is the World Wide Web Consortium (W3C), a full-time staff, and the general public that collaborates to develop Web standards.`,
+    },
+  ];
